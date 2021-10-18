1b9bb8ab6c3dfd1ffb8d78c566c69972d8b587ca
ChiriH <79470220+ChiriH@users.noreply.github.com>
2021-10-18 14:24:53 -0600

    Add files via upload

--- js/peli.js
+++ js/peli.js
@@ -1,57 +1,51 @@
-var nombres = {
-    "Personas": [
-        {"imagen" : "img/juan-robles.jpg",
-         "nombre" : "Juan",
-         "apellido" : "Robles",
-         "edad" : 29,
-         "profesion" : "Licenciado"},
-        {"imagen" : "img/lilian-rosales.jpg",
-         "nombre" : "Lilian",
-         "apellido" : "Rosales",
-         "edad" : 27,
-         "profesion" : "Bachiller"},
-        {"imagen" : "img/gustavo-gonzalez.jpg",
-         "nombre" : "Gustavo",
-         "apellido" : "Gonzalez",
-         "edad" : 28,
-         "profesion" : "Arquitecto"},
-        {"imagen" : "img/genesis-deras.jpg",
-         "nombre" : "Génesis",
-         "apellido" : "Deras",
-         "edad" : 31,
-         "profesion" : "Mercadóloga"},
-        {"imagen" : "img/reina-benitez.jpg",
-         "nombre" : "Reina",
-         "apellido" : "Benitez",
-         "edad" : 25,
-         "profesion" : "Secretaria"},
-        {"imagen" : "img/hugo-perez.jpg",
-         "nombre" : "Hugo",
-         "apellido" : "Pérez",
-         "edad" : 34,
-         "profesion" : "Diseñador"}
-    ]
+var pelis = {
+    "Peliculas": [
+        {"imagen" : "img/venom.jpg",
+         "Pelicula" : "Venom 2",
+         "Clasificacion" : "A",
+         "Duracion" : 90,
+         "Horarios" : "10 a.m | 12 a.m | 1 p.m",
+         "Butacas" : "Tradicionales",
+         "link" : "compra.html"},
+        {"imagen" : "img/spider.jpg",
+         "Pelicula" : "Spider-man: no way home",
+         "Clasificacion" : "B",
+         "Duracion" : 120,
+         "Horarios" : "7 a.m | 10 a.m | 12 p.m",
+         "Butacas" : "Tradicionales",
+         "link" : "compra.html"},
+        {"imagen" : "img/end.jpg",
+         "Pelicula" : "Avengers: Endgame",
+         "Clasificacion" : "C",
+         "Duracion" : 190,
+         "Horarios" : "11 a.m | 2 p.m | 6 p.m",
+         "Butacas" : "Tradicionales",
+         "link" : "compra.html"},
+         ]
 };
 
 //Obteniendo el elemento contenedor donde se cargarán
 //todos los datos del objeto JSON
 var div = document.getElementById("info");
-div.innerHTML = volcarDatos(nombres.Personas);
+div.innerHTML = volcarDatos(pelis.Peliculas);
 
 function volcarDatos(datos){
     var total = datos.length;
-    data = "<ul class=\"grid\">\n";
+    data = "<div class=\"grid\">\n";
     for(var i=0; i<total; i++){
-        data += "<li class=\"box\">\n"; 
-        data += "<div class=\"box-shadow\"></div>\n";
-        data += "<div class=\"box-gradient\">\n";
-        data += "<img src=\"" + datos[i].imagen + "\" alt=\"Avatar de " + datos[i].nombre + " " + datos[i].apellido + "\" class=\"avatar\" />\n";
-        data += "<h4>\nNombre: " + datos[i].nombre + " " + datos[i].apellido + "\n</h4>\n";
-        data += "<p>\nEdad: " + datos[i].edad + "\n<br />\n";
-        data += "Profesión: " + datos[i].profesion + "\n</p>\n";
+        data += "<div class=\"cards\">\n"; 
+            data += "<div class=\"text image\">\n";
+                data += "<img width=\"300px\" src=\"" + datos[i].imagen + "\" alt=\"Imagen de " + datos[i].Pelicula + "\" class=\"avatar\" />\n";
+            data += "</div>\n";
+            data += "<div class=\"text\">\n";
+                data += "<h4>\nPelícula: " + datos[i].Pelicula + "\n</h4>\n";
+                data += "<p>\nDuración (min): " + datos[i].Duracion + "\n<br />\n";
+                data += "Horarios: " + datos[i].Horarios+ "\n<br>\n";
+                data += "Butacas: " + datos[i].Butacas+ "\n</p>\n";
+                 data += "<a href="+ datos[i].link+">Comprar</a> " ;
+            data += "</div>\n";
         data += "</div>\n";
-        data += "</li>\n";
     }
-    data += "</ul>\n";
+    data += "</div>\n";
     return data;
 }
